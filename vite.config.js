bd740c15d195c5162892b1a29a353b552e2fb070
Nikoloz Pairazian <nika.pairazian@gmail.com>
2025-03-05 04:28:27 +0400

    Fix paths and add configuration for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
     base: "/IT-school-landing/",
+    build: {
+        outDir: "dist",
+        assetsDir: "assets",
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+            },
+            output: {
+                manualChunks: undefined,
+            },
+        },
+    },
+    resolve: {
+        alias: {
+            "@": resolve(__dirname, "src"),
+        },
+    },
 });
